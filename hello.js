d99fd2ad1276b6bbd50161db05f9e31d52551468
Allenkuo <scf0309@yahoo.com.tw>
2022-12-04 15:45:47 +0800

    新增js資料夾callback.js

--- hello.js
+++ hello.js
@@ -1,1 +1,25 @@
 console.log('hello world');
+
+let [name,setName]=useState("abc");
+
+setName("def")
+
+
+
+let [orders,setOrders]=useState([]);
+
+setOrders([])
+
+let newOrders=[...orders];
+//...代表撈
+//{}要一塊heep空間
+newOrders.push();
+setOrders(newOrders);
+
+
+let str = JSON.stringify(person);
+console.log(str);
+let another =JSON.parse(str);
+person.age = 28;
+console.log('another', another);
+
--- sum.js
+++ sum.js
@@ -1,3 +1,6 @@
+//O(1)
+
+
 function sum(n) {
     // 回傳 1+2+3+4+...+n 的結果
     let sum=0;
@@ -7,7 +10,13 @@ function sum(n) {
     return sum;
   }
   
-  console.log(sum(1)); // 1
-  console.log(sum(2)); // 3
-  console.log(sum(3)); // 6
-  console.log(sum(10)); // 55
+//   console.log(sum(1)); // 1
+//   console.log(sum(2)); // 3
+//   console.log(sum(3)); // 6
+//   console.log(sum(10)); // 55
+
+console.time('SUM1');
+for(let i=0; i<=10000; i++){
+    SUM1(10000);
+}
+console.timeEnd('SUM!')
